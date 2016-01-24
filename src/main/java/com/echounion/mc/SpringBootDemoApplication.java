e250fda0d5587c37ab4fbfb108aa9781bf78ae02
phuang <leo.peng@qq.com>
2016-01-24 17:08:14 +0800

    初始化项目

--- src/main/java/com/echounion/mc/SpringBootDemoApplication.java
+++ src/main/java/com/echounion/mc/SpringBootDemoApplication.java
@@ -1,8 +1,10 @@
 package com.echounion.mc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.echounion.mc.**.dao")
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
--- src/test/java/com/echounion/mc/SpringBootDemoApplicationTests.java
+++ src/test/java/com/echounion/mc/SpringBootDemoApplicationTests.java
@@ -1,18 +1,27 @@
 package com.echounion.mc;
 
+import com.echounion.mc.modules.sys.entity.User;
+import com.echounion.mc.modules.sys.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootDemoApplication.class)
 @WebAppConfiguration
 public class SpringBootDemoApplicationTests {
 
+	@Resource
+	private UserService userService;
+
 	@Test
 	public void contextLoads() {
+		User user = userService.findByUsername("admin");
+		System.out.println("");
 	}
 
 }
