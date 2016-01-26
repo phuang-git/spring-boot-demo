0f46b093ee5b06835e4e4952ced2c9a893e367f3
phuang <leo.peng@qq.com>
2016-01-26 09:42:46 +0800

    集成shiro

--- src/main/java/com/echounion/mc/common/config/ShiroConfiguration.java
+++ src/main/java/com/echounion/mc/common/config/ShiroConfiguration.java
@@ -3,10 +3,15 @@ package com.echounion.mc.common.config;
 import com.echounion.mc.modules.sys.security.SecurityRealm;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.session.mgt.eis.JavaUuidSessionIdGenerator;
+import org.apache.shiro.session.mgt.quartz.QuartzSessionValidationScheduler;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
@@ -59,10 +64,58 @@ public class ShiroConfiguration {
         return daap;
     }
 
+    //会话ID生成器
+    @Bean(name = "javaUuidSessionIdGenerator")
+    public JavaUuidSessionIdGenerator getJavaUuidSessionIdGenerator() {
+        return new JavaUuidSessionIdGenerator();
+    }
+
+    //会话DAO
+    @Bean(name = "enterpriseCacheSessionDAO")
+    public EnterpriseCacheSessionDAO getEnterpriseCacheSessionDAO() {
+        EnterpriseCacheSessionDAO enterpriseCacheSessionDAO = new EnterpriseCacheSessionDAO();
+        enterpriseCacheSessionDAO.setActiveSessionsCacheName("shiro-activeSessionCache");
+        enterpriseCacheSessionDAO.setSessionIdGenerator(getJavaUuidSessionIdGenerator());
+        return enterpriseCacheSessionDAO;
+    }
+
+    //会话Cookie模板
+    @Bean(name = "simpleCookie")
+    public SimpleCookie getSimpleCookie() {
+        SimpleCookie simpleCookie = new SimpleCookie("mc.session.id");
+        simpleCookie.setHttpOnly(true);
+        simpleCookie.setMaxAge(-1);
+        return simpleCookie;
+    }
+
+    //会话验证调度器
+    @Bean(name = "quartzSessionValidationScheduler")
+    public QuartzSessionValidationScheduler getQuartzSessionValidationScheduler() {
+        QuartzSessionValidationScheduler quartzSessionValidationScheduler = new QuartzSessionValidationScheduler();
+        quartzSessionValidationScheduler.setSessionValidationInterval(1800000);
+        quartzSessionValidationScheduler.setSessionManager(getDefaultWebSessionManager());
+        return quartzSessionValidationScheduler;
+    }
+
+    // 会话管理器
+    @Bean(name="defaultWebSessionManager")
+    public DefaultWebSessionManager getDefaultWebSessionManager(){
+        DefaultWebSessionManager defaultWebSessionManager = new DefaultWebSessionManager();
+        defaultWebSessionManager.setGlobalSessionTimeout(1800000);
+        defaultWebSessionManager.setDeleteInvalidSessions(true);
+        defaultWebSessionManager.setSessionValidationSchedulerEnabled(true);
+//        defaultWebSessionManager.setSessionValidationScheduler(getQuartzSessionValidationScheduler());
+        defaultWebSessionManager.setSessionDAO(getEnterpriseCacheSessionDAO());
+        defaultWebSessionManager.setSessionIdCookie(getSimpleCookie());
+        defaultWebSessionManager.setSessionIdCookieEnabled(true);
+        return defaultWebSessionManager;
+    }
+
     @Bean(name = "securityManager")
     public DefaultWebSecurityManager getDefaultWebSecurityManager() {
         DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
         dwsm.setRealm(getShiroRealm());
+        dwsm.setSessionManager(getDefaultWebSessionManager());
         dwsm.setCacheManager(getEhCacheManager());
         return dwsm;
     }
